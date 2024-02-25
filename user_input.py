e8d6bb78f66c1181e4057e4fd890285320e17698
fred-maina <fchege04@gmail.com>
2024-02-25 14:53:26 +0300

    Added comments

--- user_input.py
+++ user_input.py
@@ -1,6 +1,7 @@
 def main():
-    name=input("Input Your Name: ")
-    age=int(input("Input Your Age:"))
-    Location=input("Input your Location: ")
-    print(f"Hello, {name}. You are {age} years old, and you come from {Location}.")
+    #main function of the program
+    name=input("Input Your Name: ")#asks user for their name
+    age=int(input("Input Your Age:"))# Takes users input for age and converts it to an integer
+    Location=input("Input your Location: ")#takes user input for location and saves it in location
+    print(f"Hello, {name}. You are {age} years old, and you come from {Location}.")#uses string formatting to output the details collected from the user
 main()
