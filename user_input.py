ab3631695f8bb7d0bcb843653ee80c31880cc789
fred-maina <fchege04@gmail.com>
2024-02-25 14:49:52 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+def main():
+    name=input("Input Your Name: ")
+    age=int(input("Input Your Age:"))
+    Location=input("Input your Location: ")
+    print(f"Hello, {name}. You are {age} years old, and you come from {Location}.")
+main()
